9566260dd62fe781e709de113093cca7478fe222
TheSumsito <marcelo.jara@teamcore.cl>
2025-04-01 13:41:51 -0300

    update bootstrap console log message

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
 
   await app.listen(envs.port);
 
-  console.log('hola mundo - primer cambio')
+  console.log('hola mundo - primer cambio!!')
 
   logger.log(`Gateway running on port ${envs.port}`);
 }
